31a0aef74c5f64e37d07c591bc435f2e8cd582c7
Joseph Bennett <josephbennett42@gmail.com>
2015-06-13 12:38:54 +1200

    add art

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,97 @@
 require 'set'
 
+$art = [
+  <<-ART ,
+ ____________
+ |/       |
+ |
+ |
+ |
+ |
+ |
+_|___
+  ART
+  <<-ART ,
+ ____________
+ |/       |
+ |      (._.)
+ |
+ |
+ |
+ |
+_|___
+  ART
+  <<-ART ,
+ ____________
+ |/       |
+ |      (._.)
+ |        |
+ |
+ |
+ |
+_|___
+  ART
+  <<-ART ,
+ ____________
+ |/       |
+ |      (._.)
+ |       /|\\
+ |
+ |
+ |
+_|___
+  ART
+  <<-ART ,
+ ____________
+ |/       |
+ |      (._.)
+ |       /|\\
+ |        |
+ |
+ |
+_|___
+  ART
+  <<-ART ,
+ ____________
+ |/       |
+ |      (._.)
+ |       /|\\
+ |        |
+ |       / \\
+ |
+_|___
+  ART
+  <<-ART ,
+ ____________
+ |/       |
+ |      (x_.)
+ |       /|\\
+ |        |
+ |       / \\
+ |
+_|___
+  ART
+    <<-ART ,
+ ____________
+ |/       |
+ |      (x_x)
+ |       /|\\
+ |        |
+ |       / \\
+ |
+_|___
+  ART
+
+]
+
+STARTING_LIVES = 7
+
 def print_welcome
     puts "=== RailsN00bs Hangman ==="
+end
+
+def print_man_being_hanged(lives_left)
+    puts $art[STARTING_LIVES - lives_left]
 end
 
 def print_word(word, guessed_letters)
@@ -9,33 +99,33 @@ def print_word(word, guessed_letters)
 end
 
 def play_game
-    lives_left = 7
+    lives_left = STARTING_LIVES
     guessed_letters = Set.new()
     word_to_guess = File.readlines('words.txt').map { |word| word.strip }.sample
-   
-    puts word_to_guess 
+
+    puts word_to_guess
     print_welcome()
 
     until word_to_guess.chars.all?{ |c| guessed_letters.include?(c) } or lives_left == 0
         puts "Lives left: #{lives_left}"
+        print_man_being_hanged(lives_left)
         print_word(word_to_guess, guessed_letters)
-        
+
         guessed_letter = gets.strip
         guessed_letters.add(guessed_letter)
-        
+
         if word_to_guess.include? guessed_letter
             puts "That's right!"
         else
             lives_left -= 1
-        end    
+        end
     end
-    
+
     if lives_left > 0
         puts "You win!"
     else
         puts "You lost... better luck next time!"
     end
-end   
+end
 
-play_game() 
-    
+play_game()
